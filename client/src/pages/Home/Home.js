3ae2ea58de055fbb0f822692ecdc276a83ba2fb2
jfleck91 <jfleck91@gmail.com>
2018-07-13 15:44:00 -0700

    home page working

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,13 +1,42 @@
 import React, { Component } from "react";
 import Navbar from "../../components/Navbar";
-
+import { Row } from 'react-bootstrap';
+import { Grid } from 'react-bootstrap';
+import { Col } from 'react-bootstrap';
+import { Thumbnail } from 'react-bootstrap';
+import {  } from 'react-bootstrap';
+import {  } from 'react-bootstrap';
 
 const Home = () => (
 
     <div>
     <Navbar />
-    
-        <h1> Test Home Page </h1>
+    <br /><br />
+    <Grid>
+     <Row>
+    <Col xs={6} md={3}>
+      <Thumbnail href="https://www.healthgrades.com/find-a-doctor" alt="171x180" img src={require('../../images/Medical.jpg')}/>
+      <p>Find A Doctor</p>
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail href="https://www.mayoclinic.org/symptom-checker/select-symptom/itt-20009075" alt="171x180" img src={require('../../images/sick.jpg')}/>
+      <p>Check Symptoms</p>
+    </Col>
+    <Col xs={6} md={3}>
+      <Thumbnail href="https://www.goodrx.com/"  img src={require('../../images/pills.jpg')}/>
+      <p>Low Cost RX</p>
+    </Col>
+  </Row>
+
+
+<br /><br /><br />
+
+<h2> About Us</h2>
+<p>info to come</p>
+
+
+
+</Grid>;
     </div>
     );
 
