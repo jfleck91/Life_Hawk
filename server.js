5d40e6abb2da914202fe09bc106848eaf4b66b7f
jfleck91 <jfleck91@gmail.com>
2018-07-16 11:50:35 -0700

    deploy attempt

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require ("express")
+var app = express()
+var port = process.env.PORT||3000
+var path = require ("path")
+app.use(express.static("client/build"))
+//app.get('*', function response(req, res) {
+  //  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'client/build/index.html')));
+    //res.end();
+  //});
+
+app.get("*",function (req,res){
+    res.sendFile(path.join(__dirname,"client/build/index.html"))
+})
+
+app.listen(port)
