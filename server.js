c514ae3bba22ded6996d82649494398c48fe9b87
jfleck91 <jfleck91@gmail.com>
2018-07-16 16:44:01 -0700

    4

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var app = express()
 var port = process.env.PORT||3000
 var path = require ("path")
 app.use(express.static("client/build"))
+
 //app.get('*', function response(req, res) {
   //  res.write(middleware.fileSystem.readFileSync(path.join(__dirname, 'client/build/index.html')));
     //res.end();
   //});
 
-app.get("*",function (req,res){
+  app.get("*",function (req,res){
     res.sendFile(path.join(__dirname,"client/build/index.html"))
 })
 
