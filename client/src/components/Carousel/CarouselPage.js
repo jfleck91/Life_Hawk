243b98b270550231e9cb8205e581687bf8b843e5
jfleck91 <jfleck91@gmail.com>
2018-07-16 18:33:56 -0700

    7

--- client/src/components/Carousel/CarouselPage.js
+++ client/src/components/Carousel/CarouselPage.js
@@ -4,8 +4,10 @@ import  {Carousel, CarouselCaption, CarouselInner, CarouselItem, View, Mask, Con
 class CarouselPage extends Component {
   render(){
     return(
+      
       <Container>
-        <h4 className="mt-5 mb-2">Basic example</h4>
+        <br/>
+        <h4 className="mt-5 mb-2">Welcome!</h4>
         <Carousel
           activeItem={1}
           length={4}
@@ -15,47 +17,34 @@ class CarouselPage extends Component {
           <CarouselInner>
             <CarouselItem itemId="1">
               <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(68).jpg" alt="First slide" />
+                <img className="d-block w-100" img src={require('../../images/logo2.png')}  alt="First slide" />
                 <Mask overlay="black-light"></Mask>
               </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Light mask</h3>
-                <p>First text</p>
-              </CarouselCaption>
             </CarouselItem>
             <CarouselItem itemId="2">
               <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(99).jpg" alt="Second slide" />
+                <img className="d-block w-100" img src={require('../../images/logo3.jpg')}  alt="Second slide" />
                 <Mask overlay="black-strong"></Mask>
               </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Strong mask</h3>
-                <p>Second text</p>
-              </CarouselCaption>
+        
             </CarouselItem>
             <CarouselItem itemId="3">
               <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(17).jpg" alt="Third slide" />
+                <img className="d-block w-100" img src={require('../../images/logo2.png')}  alt="Third slide" />
                 <Mask overlay="black-slight"></Mask>
               </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Slight mask</h3>
-                <p>Third text</p>
-              </CarouselCaption>
             </CarouselItem>
             <CarouselItem itemId="4">
               <View>
-                <img className="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20%28143%29.jpg" alt="Mattonit's item" />
+                <img className="d-block w-100" img src={require('../../images/logo4.jpg')}  alt="Mattonit's item" />
                 <Mask overlay="black-light"></Mask>
               </View>
-              <CarouselCaption>
-                <h3 className="h3-responsive">Sopot Beach</h3>
-                <p>Taken june 21th by @mattonit</p>
-              </CarouselCaption>
+      
             </CarouselItem>
           </CarouselInner>
         </Carousel>
       </Container>
+    
     );
   }
 }
